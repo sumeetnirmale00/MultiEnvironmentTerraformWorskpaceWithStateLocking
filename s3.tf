69f89819ad18495b924fd295698174bec8f42c96
Sumeet Nirmale <sumeet.nirmale00@gmail.com>
2025-06-18 17:35:15 +0530

    Dev Code Update

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "terraform_state_bucket" {
-  bucket = "sumeet-terraform-backend-bucket"
+# resource "aws_s3_bucket" "terraform_state_bucket" {
+#   bucket = "sumeet-terraform-backend-bucket"
 
-  tags = {
-    Name        = "Terraform Backend Bucket"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "Terraform Backend Bucket"
+#     Environment = "Dev"
+#   }
+# }
